2dab3c8532e2367595889d2f734ca4537b169bd2
iflar <classicsience@gmail.com>
2022-01-06 20:05:17 -0500

    Add project files.

---
+++ ResturantRaterMVC/Models/Restaurant.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ResturantRaterMVC.Models
+{
+    public class Restaurant
+    {
+        public int RestaurantID { get; set; }
+        public string Name { get; set; }
+        public string Adress { get; set; }
+        public int Rating { get; set; }
+        
+    }
+
+    public class RestaurantDbContex : DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
---
+++ ResturantRaterMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ResturantRaterMVC.Startup))]
+namespace ResturantRaterMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
